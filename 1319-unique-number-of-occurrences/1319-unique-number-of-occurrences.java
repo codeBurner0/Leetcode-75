25be01f6b91262943d99d41e2be07adf7b4b0392
Ankit Anand <106039683+codeBurner0@users.noreply.github.com>
2023-05-20 13:58:31 +0530

    Time: 2 ms (93.57%), Space: 40.9 MB (45.63%) - LeetHub

---
+++ 1319-unique-number-of-occurrences/1319-unique-number-of-occurrences.java
@@ -0,0 +1,27 @@
+class Solution {
+    public boolean uniqueOccurrences(int[] nums) {
+        HashMap<Integer,Integer> map= new HashMap<>();
+        HashSet<Integer> set=new HashSet<>();
+        for(int i=0;i<nums.length;i++){
+            if(map.containsKey(nums[i])){
+                map.put(nums[i],map.get(nums[i])+1);
+                nums[i]=0;
+            }else{
+                map.put(nums[i],1);
+            }
+        }
+        for(int i=0;i<nums.length;i++){
+            if(nums[i]!=0){
+                if(set.contains(map.get(nums[i]))){
+                return false;
+            }else{
+                set.add(map.get(nums[i]));
+            }
+            }
+            
+        }
+        return true;
+    }
+}
+
+// map.put(x, map.getOrDefault(x, 0) + 1);
