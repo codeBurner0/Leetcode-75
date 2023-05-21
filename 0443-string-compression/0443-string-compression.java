7e5a799befa7d94e27caa560bb902884a1818890
Ankit Anand <106039683+codeBurner0@users.noreply.github.com>
2023-05-21 15:41:56 +0530

    Time: 1 ms (96.79%), Space: 43.3 MB (5.49%) - LeetHub

---
+++ 0443-string-compression/0443-string-compression.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int compress(char[] nums) {
+        int n=nums.length;
+        if(n==1){
+            return 1;
+        }
+        int i=0; int j=0;
+        while(i<n){
+            int count=1;
+            while(i<(n-1) && nums[i]==nums[i+1]){
+                count++;
+                i++;
+            }
+            nums[j++]=nums[i++];
+            if(count>1){
+                String str=Integer.toString(count);
+                int len=str.length();
+                for(int k=0;k<len;k++){
+                nums[j]=str.charAt(k);
+                j++;
+                }
+            }
+
+        }
+        return j;
+    }
+}
