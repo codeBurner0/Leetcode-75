90220cf6ea23f1438d8156bf82a959903254bb34
Ankit Anand <106039683+codeBurner0@users.noreply.github.com>
2023-05-22 09:46:07 +0530

    Time: 5 ms (63.68%), Space: 44.1 MB (15.19%) - LeetHub

--- 0345-reverse-vowels-of-a-string/0345-reverse-vowels-of-a-string.java
+++ 0345-reverse-vowels-of-a-string/0345-reverse-vowels-of-a-string.java
@@ -1,7 +1,41 @@
 class Solution {
+    // public String reverseVowels(String s) {
+        // HashSet<Character> set=new HashSet<>();
+        // set.add('a');
+        // set.add('e');
+        // set.add('i');
+        // set.add('o');
+        // set.add('u');
+        // set.add('A');
+        // set.add('E');
+        // set.add('I');
+        // set.add('O');
+        // set.add('U');
+
+    //     ArrayList<Character> al=new ArrayList<>();
+    //     String ans="";
+    //     for(int i=0;i<s.length();i++){
+    //         if(set.contains(s.charAt(i))){
+    //             al.add(s.charAt(i));
+    //         }
+    //     }
+    //     int j=al.size()-1;
+    //     for(int i=0;i<s.length();i++){
+    //         if(set.contains(s.charAt(i))){
+    //             ans+=al.get(j);
+    //             j--;
+    //         }else{
+    //             ans+=s.charAt(i);
+    //         }
+    //     }
+    //     return ans;
+    // }
+
+
     public String reverseVowels(String s) {
+        char ch[]=s.toCharArray();
         HashSet<Character> set=new HashSet<>();
-        set.add('a');
+         set.add('a');
         set.add('e');
         set.add('i');
         set.add('o');
@@ -11,23 +45,24 @@ class Solution {
         set.add('I');
         set.add('O');
         set.add('U');
-
-        ArrayList<Character> al=new ArrayList<>();
-        String ans="";
-        for(int i=0;i<s.length();i++){
-            if(set.contains(s.charAt(i))){
-                al.add(s.charAt(i));
+        int start=0;
+        int end=s.length()-1;
+        while(start<end){
+            if(!set.contains(ch[start])){
+                start++;
+            }
+            if(!set.contains(ch[end])){
+                end--;
+            }
+            if(set.contains(ch[start]) && set.contains(ch[end])){
+                char temp=ch[start];
+                ch[start]=ch[end];
+                ch[end]=temp;
+                start++;
+                end--;
             }
         }
-        int j=al.size()-1;
-        for(int i=0;i<s.length();i++){
-            if(set.contains(s.charAt(i))){
-                ans+=al.get(j);
-                j--;
-            }else{
-                ans+=s.charAt(i);
-            }
-        }
-        return ans;
+        String str=new String(ch);
+        return str;
     }
 }
