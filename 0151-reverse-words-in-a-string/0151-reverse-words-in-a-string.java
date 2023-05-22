0dd4cc659dc787175776d6fcab621ce95df5aef9
Ankit Anand <106039683+codeBurner0@users.noreply.github.com>
2023-05-22 10:07:27 +0530

    Time: 11 ms (29.52%), Space: 44.4 MB (5.01%) - LeetHub

--- 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.java
+++ 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.java
@@ -1,38 +1,38 @@
 class Solution {
-    // public String reverseWords(String s) {
-    //     String arr[]=s.trim().split(" ");
-    //     String ans="";
-    //     for(int i=arr.length-1;i>0;i--){
-    //         if(arr[i].length()!=0)
-    //         ans+=arr[i].trim()+" ";
-    //     }
-    //     ans+=arr[0].trim();
-    //     return ans;
-    // }
-
     public String reverseWords(String s) {
-        Stack<String> stack=new Stack<>();
-        String temp="";
-        String res="";
-        for(int i=0;i<s.length();i++){
-            if(s.charAt(i)!=' '){
-                temp+=s.charAt(i);
-            }else{
-                if(temp.length()>0){
-                    stack.push(temp);
-                    temp="";
-                }
-            }
+        String arr[]=s.trim().split(" ");
+        String ans="";
+        for(int i=arr.length-1;i>0;i--){
+            if(arr[i].length()!=0)
+            ans+=arr[i].trim()+" ";
         }
-        res=res+temp;
-        while(!stack.isEmpty()){
-            res+=" ";
-            res+=stack.pop();
-            
-        }
-        if(res.charAt(0)==' ' && res.length()>0){
-            return res.substring(1);
-        }
-        return res;
+        ans+=arr[0].trim();
+        return ans;
     }
+
+    // public String reverseWords(String s) {
+    //     Stack<String> stack=new Stack<>();
+    //     String temp="";
+    //     String res="";
+    //     for(int i=0;i<s.length();i++){
+    //         if(s.charAt(i)!=' '){
+    //             temp+=s.charAt(i);
+    //         }else{
+    //             if(temp.length()>0){
+    //                 stack.push(temp);
+    //                 temp="";
+    //             }
+    //         }
+    //     }
+    //     res=res+temp;
+    //     while(!stack.isEmpty()){
+    //         res+=" ";
+    //         res+=stack.pop();
+            
+    //     }
+    //     if(res.charAt(0)==' ' && res.length()>0){
+    //         return res.substring(1);
+    //     }
+    //     return res;
+    // }
 }
